a73a1554d8770c415da31fca52e4e3c0cba16b9c
Kushalgt <110596956+Kushalgt@users.noreply.github.com>
2024-03-04 18:40:11 +0530

    added new

--- home/views.py
+++ home/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse  # Fix the import statement
 def home(request):
    labels = ['32', '34', '36', '38', '40','42','44','46']
    values = [32,32, 33, 33, 33.5, 34, 33,35]
-   return render(request,'index.html', {'labels': labels, 'values': values})
+   average=0
+   for value in values:
+      average+=value
+   average/=len(values)
+   return render(request,'index.html', {'labels': labels, 'values': values,'averages' : average})
