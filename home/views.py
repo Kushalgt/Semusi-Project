f50cf77262986234d715b83df83b50c012de248b
Kushalgt <110596956+Kushalgt@users.noreply.github.com>
2024-03-04 18:26:42 +0530

    added channel name

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse  # Fix the import statement
 # Create your views here.
 def home(request):
    labels = ['32', '34', '36', '38', '40','42','44','46']
-   values = [30,31, 32, 33, 36, 37, 40,44]
+   values = [32,32, 33, 33, 33.5, 34, 33,35]
    return render(request,'index.html', {'labels': labels, 'values': values})
